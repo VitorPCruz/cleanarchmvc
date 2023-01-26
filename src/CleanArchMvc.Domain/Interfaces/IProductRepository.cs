ab7b98edbe6e4a3e4baf2093e2bf2199ad226d79
Vitor Pereira <vitor-pcruz@hotmail.com>
2023-01-26 19:05:54 -0300

    Criando interfaces de repositórios para Product e Category

---
+++ src/CleanArchMvc.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchMvc.Domain.Entities;
+
+namespace CleanArchMvc.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetCategoriesAsync();
+        Task<Product> GetByIdAsync(int? id);
+        Task<IEnumerable<Product>> GetProductCategoryAsync(int? id);
+        Task<Product> CreateAsync(Product category);
+        Task<Product> UpdateAsync(Product category);
+        Task<Product> RemoveAsync(Product category);
+    }
+}
