095522201f36c05d91a956639f0b39a23c3ada9d
Vitor Pereira <vitor-pcruz@hotmail.com>
2023-01-28 16:23:02 -0300

    implementando interfaces de serviço para product e category; implementando classe concreta de serviço para category

---
+++ src/CleanArchMvc.Application/Interfaces/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using CleanArchMvc.Application.DTOs;
+
+namespace CleanArchMvc.Application.Interfaces;
+
+public interface IProductService
+{
+    Task<IEnumerable<ProductDTO>> GetProductsAsync();
+    Task<ProductDTO> GetByIdAsync(int? id);
+    Task<ProductDTO> GetProductCategoryAsync(int? id);
+    Task AddAsync(ProductDTO product);
+    Task UpdateAsync(ProductDTO product);
+    Task RemoveAsync(int? id);
+}
