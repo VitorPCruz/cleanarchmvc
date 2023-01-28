16792861447c5295b4e3188e032b18580796088a
Vitor Pereira <vitor-pcruz@hotmail.com>
2023-01-28 15:17:15 -0300

    gerando migracoes das entidades e criando banco de dados; gerando scripts SQL Server das migracoes

---
+++ sql/Incial_Criacao_Db.sql
@@ -0,0 +1,22 @@
+﻿BEGIN TRANSACTION;
+GO
+
+INSERT INTO Products(Name,Description,Price,Stock,Image,CategoryId)VALUES('Caderno Espiral','Caderno espiral 100 folhas',7.45,50,'caderno1.jpg',1)
+GO
+
+INSERT INTO Products(Name,Description,Price,Stock,Image,CategoryId)VALUES('Estojo escolar','Estojo escolar cinza',5.65,70,'estojo1.jpg',1)
+GO
+
+INSERT INTO Products(Name,Description,Price,Stock,Image,CategoryId)VALUES('Borracha escolar','Borracha branca pequena',3.25,80,'borracha1.jpg',1)
+GO
+
+INSERT INTO Products(Name,Description,Price,Stock,Image,CategoryId)VALUES('Calculadora escolar','Calculadora escolar cinza',15.39,20,'calculadora1.jpg',1)
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20230128153729_SeedProducts', N'7.0.2');
+GO
+
+COMMIT;
+GO
+
