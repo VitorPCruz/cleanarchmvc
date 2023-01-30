993e9b62bdafed62451461c2454f64c4fb55ec70
Vitor Pereira <vitor-pcruz@hotmail.com>
2023-01-30 08:48:09 -0300

    adicionando products controller em webui

---
+++ src/CleanArchMvc.WebUI/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using CleanArchMvc.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchMvc.WebUI.Controllers;
+
+public class ProductsController : Controller
+{
+    public IProductService _productService;
+
+    public ProductsController(IProductService productService)
+    {
+        _productService = productService;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var products = await _productService.GetProductsAsync();
+        return View(products);
+    }
+}
