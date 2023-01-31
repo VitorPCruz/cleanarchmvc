1d0434beff3899ec0abdfc473bc657bbb341f1be
Vitor Pereira <vitor-pcruz@hotmail.com>
2023-01-31 11:24:18 -0300

    incluindo serviços do identity e registrando os serviços no projeto infra.ioc

--- src/CleanArchMvc.Infra.IoC/DependencyInjection.cs
+++ src/CleanArchMvc.Infra.IoC/DependencyInjection.cs
@@ -1,11 +1,15 @@
 using CleanArchMvc.Application.Interfaces;
 using CleanArchMvc.Application.Mappings;
 using CleanArchMvc.Application.Services;
+using CleanArchMvc.Domain.Account;
 using CleanArchMvc.Domain.Interfaces;
 using CleanArchMvc.Infra.Data.Context;
+using CleanArchMvc.Infra.Data.Identity;
 using CleanArchMvc.Infra.Data.Repositories;
 using MediatR;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -15,10 +19,25 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfraestructure(this IServiceCollection services, IConfiguration configuration)
     {
+        #region Database
         services.AddDbContext<ApplicationDbContext>(
             options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
             b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+        #endregion
 
+        #region Identity
+        services.AddIdentity<ApplicationUser, IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
+
+        services.AddScoped<IAuthenticate, AuthenticateService> ();
+        services.AddScoped<ISeedUserRoleInitial, SeedUserRoleInitial>();
+
+        services.ConfigureApplicationCookie(options =>
+            options.AccessDeniedPath = "/Account/Login");
+        #endregion
+
+        #region Services
         services.AddScoped<ICategoryRepository, CategoryRepository>();
         services.AddScoped<IProductRepository, ProductRepository>();
 
@@ -29,7 +48,8 @@ public static class DependencyInjection
 
         var myHandlers = AppDomain.CurrentDomain.Load("CleanArchMvc.Application");
         services.AddMediatR(myHandlers);
-        
+        #endregion
+
         return services;
     }
 }
